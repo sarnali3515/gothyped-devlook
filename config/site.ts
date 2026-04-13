7000f9794f62027fab4f039a3677df09afe05d95
sarnali3515 <191sarnali@gmail.com>
2026-04-13 21:39:51 +0600

    first commit

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
+  name: "Get Hyped - Social-first content Agency",
   description: "Make beautiful websites regardless of your design experience.",
   navItems: [
     {
