ce27bc8e64998b1ef61a96f37404543fe339a69a
sarnali3515 <191sarnali@gmail.com>
2026-04-19 20:23:10 +0600

    fix UI issues

--- components/home/ContentSection.jsx
+++ components/home/ContentSection.jsx
@@ -64,7 +64,7 @@ export default function ContentSection() {
                                 behavior: "smooth",
                             });
                         }}
-                        className="w-12 h-12 rounded-xl border border-black flex items-center justify-center text-[#fa5424] text-xl font-bold overflow-hidden relative group cursor-pointer hover:rounded-lg"
+                        className="hidden w-12 h-12 rounded-xl border border-black lg:flex items-center justify-center text-[#fa5424] text-xl font-bold overflow-hidden relative group cursor-pointer hover:rounded-lg"
                     >
                         <div className="transform transition-all duration-300 ease-out translate-y-0 group-hover:translate-y-8">
                             ↓
